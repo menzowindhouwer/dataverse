aa9439a7ae39e5674b2c76a1d7c51e9bf0e9464c
Oliver Bertuch <o.bertuch@fz-juelich.de>
2021-06-23 14:06:27 +0200

    fix(container): make JDWP and JRebel support work again
    - JDWP is now enabled via the asadmin "--debug" switch, starting a JWDP
      server on port 9009
    - JRebel is added via agentpath again.
    - init_3_enable_dev.sh is shellchecked (not in CI, but well)
    - Running the Dataverse container via Maven automatically activates JDWP
      and JRebel support now, still using the ENABLE_JDWP and ENABLE_JREBEL
      environment vars.

--- conf/container/scripts/app/init_3_enabledev.sh
+++ conf/container/scripts/app/init_3_enabledev.sh
@@ -1,46 +1,69 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ######
 # This script enables different development options, like a JMX connector
 # usable with VisualVM, JRebel hot-reload support and JDWP debugger service.
 # Enable it by adding env vars on startup (e.g. via ConfigMap)
+#
+# As this script is "sourced" from entrypoint.sh, we can manipulate env vars
+# for the parent shell before executing Payara.
 ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ######
 
 # 0. Init variables
 ENABLE_JMX=${ENABLE_JMX:-0}
 ENABLE_JDWP=${ENABLE_JDWP:-0}
 ENABLE_JREBEL=${ENABLE_JREBEL:-0}
-JDWP_PORT=${JDWP_PORT:-9009}
 
 DV_PREBOOT=${PAYARA_DIR}/dataverse_preboot
-echo "# Dataverse preboot configuration for Payara" > ${DV_PREBOOT}
+echo "# Dataverse preboot configuration for Payara" > "${DV_PREBOOT}"
 
 # 1. Configure JMX (enabled by default on port 8686, but requires SSL)
 # See also https://blog.payara.fish/monitoring-payara-server-with-jconsole
 # To still use it, you can use a sidecar container proxying or using JMX via localhost without SSL.
 if [ "x${ENABLE_JMX}" = "x1" ]; then
-  echo "Enabling JMX on 127.0.0.1:8686. You'll need a sidecar for this."
-  echo "set configs.config.server-config.admin-service.jmx-connector.system.address=127.0.0.1" >> ${DV_PREBOOT}
+  echo "Enabling unsecured JMX on 0.0.0.0:8686. You'll need a sidecar for this, as access is allowed from same machine only (without SSL)."
+  { \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.jvm=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.connector-service=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.connector-connection-pool=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.jdbc-connection-pool=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.web-services-container=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.ejb-container=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.thread-pool=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.http-service=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.security=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.jms-service=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.jersey=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.transaction-service=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.jpa=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.web-container=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.orb=HIGH"; \
+    echo "set configs.config.server-config.monitoring-service.module-monitoring-levels.deployment=HIGH"; \
+    #echo "set configs.config.server-config.admin-service.jmx-connector.system.address=127.0.0.1"; \
+    echo "set configs.config.server-config.admin-service.jmx-connector.system.security-enabled=false"; \
+  } >> "${DV_PREBOOT}"
 fi
 
-# 2. Enable JDWP (debugger)
+# 2. Enable JDWP via debugging switch
 if [ "x${ENABLE_JDWP}" = "x1" ]; then
-  echo "Enabling JDWP debugger, listening on port ${JDWP_PORT} of this container/pod."
-  echo "create-jvm-options --target=server-config \"-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JDWP_PORT}\"" >> ${DV_PREBOOT}
+  echo "Enabling JDWP remote debugging support via asadmin debugging switch."
+  export PAYARA_ARGS="${PAYARA_ARGS} --debug=true"
 fi
+
 
 # 3. Enable JRebel (hot-redeploy)
 if [ "x${ENABLE_JREBEL}" = "x1" ] && [ -s "${JREBEL_LIB}" ]; then
   echo "Enabling JRebel support with enabled remoting_plugin option."
-  echo "create-jvm-options --target=server-config \"-agentpath:${JREBEL_LIB}\"" >> ${DV_PREBOOT}
-  echo "create-system-properties rebel.remoting_plugin=true" >> ${DV_PREBOOT}
+  export JVM_ARGS="${JVM_ARGS} -agentpath:${JREBEL_LIB} -Drebel.remoting_plugin=true"
 fi
 
 # 4. Add the commands to the existing postboot file, but insert BEFORE deployment
-echo "$(cat ${DV_PREBOOT} | cat - ${PREBOOT_COMMANDS} )" > ${PREBOOT_COMMANDS}
+TMP_PREBOOT=$(mktemp)
+cat "${DV_PREBOOT}" "${PREBOOT_COMMANDS}" > "${TMP_PREBOOT}"
+mv "${TMP_PREBOOT}" "${PREBOOT_COMMANDS}"
 echo "DEBUG: preboot contains the following commands:"
 echo "--------------------------------------------------"
-cat ${PREBOOT_COMMANDS}
+cat "${PREBOOT_COMMANDS}"
 echo "--------------------------------------------------"
