808d460b812660b0a37c25a911bfa51abd93dd3d
Menzo Windhouwer <menzo@windhouwer.nl>
2024-06-12 13:16:29 +0200

    Update do.sh

--- do.sh
+++ do.sh
@@ -2,12 +2,12 @@
 
 _PWD=`pwd`
 
-cd /Users/menzowi/Documents/Projects/dataverse-aio/conf/docker-aio
+cd ./conf/docker-aio
 
 #export JAVA_HOME=`/usr/libexec/java_home`
 ./1prep.sh && \
 docker build -t dv0 -f c8.dockerfile . && \
-docker run -d -p 8083:8080 -p 8084:80 -v /Users/menzowi/Documents/Projects/dataverse-aio/conf/docker-aio/cmd/dv_cmdi_conf.xml:/opt/dv/cmdi_config.xml -e DV_CMDI_CONF=/opt/dv/cmdi_config.xml --name dv dv0
+docker run -d -p 8083:8080 -p 8084:80 -v $_PWD/conf/docker-aio/cmd/dv_cmdi_conf.xml:/opt/dv/cmdi_config.xml -e DV_CMDI_CONF=/opt/dv/cmdi_config.xml --name dv dv0
 while true; do
 	docker logs dv
     echo "WAIT FOR: Command start-domain executed successfully."
